20e70dd0431e736112475985cbec2445e0ac6707
Guy <gforceun@gmail.com>
2022-11-06 11:54:00 -0500

    completed analysis

--- app.py
+++ app.py
@@ -28,19 +28,36 @@ app = Flask(__name__)
 def welcome():
     return(
     '''
-    Welcome to the Climate Analysis API!
-    Available Routes:
-    /api/v1.0/precipitation
-    /api/v1.0/stations
-    /api/v1.0/tobs
-    /api/v1.0/temp/start/end
+    <h1>Welcome to the Climate Analysis API!</h1>
+    <h4>Available Routes:</h4>
+    <ul>
+        <li>/api/v1.0/precipitation</li>
+        <li>/api/v1.0/stations</li>
+        <li>/api/v1.0/tobs</li>
+        <li>/api/v1.0/temp/start/end</li>
+    </ul>
     ''')
 
-import app
+@app.route('/api/v1.0/precipitation')
+def precipitation():
+    return {date:prcp for date, prcp in session.query(Measurement.date,Measurement.prcp).all()}
 
-print("example __name__ = %s", __name__)
+@app.route('/api/v1.0/stations')
+def stations():
+    return {id:location for id, location in session.query(Station.station,Station.name).all()}
+
+@app.route('/api/v1.0/tobs')
+def tobs():
+    return {date:temperature for date, temperature in session.query(Measurement.date,Measurement.tobs).all()}
+
+@app.route('/api/v1.0/<start>/')
+@app.route('/api/v1.0/<start>/<end>')
+def tempRange(start, end = '2017-08-23'):
+    temp = Measurement.tobs
+    data = session.query(func.min(temp),func.max(temp),func.avg(temp)).filter((Measurement.date>=start)&(Measurement.date<=end)).all()
+
+    return jsonify(dict(data))
+
 
 if __name__ == "__main__":
-    print("example is being run directly.")
-else:
-    print("example is being imported")
+    app.run()
